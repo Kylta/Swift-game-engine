21c0c7b17b29e3dffdde520e0ce1c42429a69509
cbugnon <christophe.bugnon@musicworldmedia.com>
2020-05-09 15:36:59 +0200

    Create Hello Triangle

---
+++ swift-metal-engine/swift-metal-engine/Helpers/SIMD+extensions.swift
@@ -0,0 +1,13 @@
+//
+//  SIMD+extensions.swift
+//  swift-metal-engine
+//
+//  Created by Christophe Bugnon on 5/9/20.
+//  Copyright © 2020 Christophe Bugnon. All rights reserved.
+//
+
+import simd
+
+typealias float2 = SIMD2<Float>
+typealias float3 = SIMD3<Float>
+typealias float4 = SIMD4<Float>
