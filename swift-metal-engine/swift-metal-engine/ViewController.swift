02568b82ea12a56d03e3c80323f4e8d99dee8f4b
cbugnon <christophe.bugnon@musicworldmedia.com>
2020-05-09 14:20:06 +0200

    Create swift-metal-engine project

---
+++ swift-metal-engine/swift-metal-engine/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  swift-metal-engine
+//
+//  Created by Christophe Bugnon on 5/9/20.
+//  Copyright © 2020 Christophe Bugnon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
